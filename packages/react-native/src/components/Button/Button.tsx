c069ad0ff8ad6313d1e8da8e68edb214384528e1
Harinder.singh <harinder.singh@newcrosshealthcare.com>
2022-03-02 16:25:27 +0000

    feat(hfp-670): setup react native project with jest and RNTL
    Approved-by: Mazaher Muraj
    Approved-by: Swetha Pravin
    Approved-by: Stephen Evans

---
+++ packages/react-native/src/components/Button/Button.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+
+const Button = () => {
+  return <TouchableOpacity>Hello</TouchableOpacity>;
+};
+
+export default Button;
