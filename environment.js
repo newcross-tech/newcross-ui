1d0810913cf88680470538ac3f08309b96988f0e
harinder.singh <harinder.singh@newcrosshealthcare.com>
2022-03-09 15:46:47 +0000

    feat(hfp-865): handle cloudfront provisioning using serverless framework

--- environment.js
+++ environment.js
@@ -1,13 +1,17 @@
 module.exports = () => {
   const region = 'eu-west-2';
+  const certificateArn =
+    'arn:aws:acm:us-east-1:${aws:accountId}:certificate/1f6caec9-ff1f-48ab-9858-141f38ba7623';
 
   const productionSetting = {
     bucketName: 'design-system.newcrosshealthcare.com',
     region,
+    certificateArn,
   };
   const acceptanceSetting = {
-    bucketName: 'acceptance.design-system.newcrosshealthcare.com',
+    bucketName: 'acceptance-design-system.newcrosshealthcare.com',
     region,
+    certificateArn,
   };
 
   return {
