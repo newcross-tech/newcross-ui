d6095dea4de9208a368065b5a0235cf04ef3e649
harinder.singh <harinder.singh@newcrosshealthcare.com>
2022-03-10 15:52:31 +0000

    refactor: change padding

--- packages/react-native/src/components/Button/Button.style.ts
+++ packages/react-native/src/components/Button/Button.style.ts
@@ -4,7 +4,7 @@ import { ButtonStyle } from './Button.types';
 const buttonStyle: StyleSheet.NamedStyles<ButtonStyle> = {
   button: {
     paddingVertical: 16,
-    paddingHorizontal: 16 * 3,
+    paddingHorizontal: 16 * 2,
     borderRadius: 16 * 10,
     flexGrow: 0,
     backgroundColor: '#0062A2',
