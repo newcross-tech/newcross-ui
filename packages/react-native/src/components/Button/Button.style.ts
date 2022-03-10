a8fb18b11e136fdf0261700cfd6c425e5f6c61de
harinder.singh <harinder.singh@newcrosshealthcare.com>
2022-03-10 16:18:21 +0000

    ci(hfp-874): add publish command in pipeline

--- packages/react-native/src/components/Button/Button.style.ts
+++ packages/react-native/src/components/Button/Button.style.ts
@@ -4,7 +4,7 @@ import { ButtonStyle } from './Button.types';
 const buttonStyle: StyleSheet.NamedStyles<ButtonStyle> = {
   button: {
     paddingVertical: 16,
-    paddingHorizontal: 16 * 2,
+    paddingHorizontal: 16 * 3,
     borderRadius: 16 * 10,
     flexGrow: 0,
     backgroundColor: '#0062A2',
