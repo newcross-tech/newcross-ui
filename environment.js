cfc6f113b79777623530cb1f896db05ece864db2
harinder.singh <harinder.singh@newcrosshealthcare.com>
2022-03-16 12:08:40 +0000

    docs(hfp-865): add route53 config for acceptance and prod

--- environment.js
+++ environment.js
@@ -1,17 +1,25 @@
 module.exports = () => {
   const region = 'eu-west-2';
   const certificateArn =
-    'arn:aws:acm:us-east-1:${aws:accountId}:certificate/1f6caec9-ff1f-48ab-9858-141f38ba7623';
+    'arn:aws:acm:us-east-1:${aws:accountId}:certificate/ca6c2f70-ca2a-42c1-8cce-50ffbad845b4';
+  const domainHostedZoneId = 'Z08576563TBGWX31WNB89';
+  const globalHostedZoneId = 'Z2FDTNDATAQYW2';
 
   const productionSetting = {
-    bucketName: 'design-system.newcrosshealthcare.com',
+    bucketName: 'www.halodesignsystem.com',
     region,
     certificateArn,
+    domainHostedZoneId,
+    globalHostedZoneId,
+    recordSet: 'www.halodesignsystem.com',
   };
   const acceptanceSetting = {
-    bucketName: 'acceptance-design-system.newcrosshealthcare.com',
+    bucketName: 'acceptance.halodesignsystem.com',
     region,
     certificateArn,
+    domainHostedZoneId,
+    globalHostedZoneId,
+    recordSet: 'acceptance.halodesignsystem.com',
   };
 
   return {
