3503e6eab2c4d35dc6969eb9b867bc27d0f03f7d
bitbucket-pipelines <commits-noreply@bitbucket.org>
2022-03-21 12:02:28 +0000

    feat(hfp-920): added poppins google font for storybooks
    feat(hfp-920): added poppins google font for storybooks
    
     - @newcross-ui/design-tokens@1.3.2
     - @newcross-ui/react-native@1.2.3
     - @newcross-stories/react-native@1.3.2
    
    feat(hfp-920): added Poppins google font
    
    fix(hfp-920): setup fonts to work within devices for storybooks
    
    refactor: radio component to have effect on press
    
    feat(hfp-915): add build watch command and upgrade to node 16
    
    chore(release): publish [skip ci]
    
     - @newcross-ui/design-tokens@1.4.0
     - @newcross-ui/react-native@1.2.4
     - @newcross-stories/react-native@1.3.3
    
    fix(hfp-920): removed second export only need the default one

--- stories/react-native/App.tsx
+++ stories/react-native/App.tsx
@@ -1,3 +1,15 @@
+import React from 'react';
+import { useFonts } from 'expo-font';
 import StorybookUIRoot from './.ondevice/Storybook';
 
-export { StorybookUIRoot as default };
+export default function App() {
+  const [loaded] = useFonts({
+    poppins: require('./assets/fonts/poppins/Poppins-Regular.ttf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
+  return <StorybookUIRoot />;
+}
