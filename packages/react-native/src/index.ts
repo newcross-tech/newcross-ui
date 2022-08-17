a812c551f619680f82b9a43cca113ee99beb2e86
harinder.singh <harinder.singh@newcrosshealthcare.com>
2022-08-17 20:20:41 +0100

    fix: add missing imports

--- packages/react-native/src/index.ts
+++ packages/react-native/src/index.ts
@@ -1,1 +1,3 @@
 export { default as Slider, SliderProps } from './components/Slider';
+export { default as ThemeProvider, ThemeContext } from './theme/ThemeProvider';
+export { default as Brand } from './theme/Brand';
