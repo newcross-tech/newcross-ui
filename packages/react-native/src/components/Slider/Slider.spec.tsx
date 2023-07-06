ea4beeb10f5979549287ebd45cc5250d7ec655ae
Mazaher Muraj <mmuraj.313@gmail.com>
2023-07-06 16:15:24 +0100

    fix(hfgo-1973): rename stories file

--- packages/react-native/src/components/Slider/Slider.spec.tsx
+++ packages/react-native/src/components/Slider/Slider.spec.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react-native';
 import Slider from './Slider';
 
-describe('SliderTwo Component', () => {
+describe('Slider Component', () => {
   it('renders successfully', () => {
     // Arrange
     const props = {
