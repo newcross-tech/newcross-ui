8ce3f2c0da7800c3380678815154306a2f9bfa6c
aapostolou <alexandros.apostolou@newcrosshealthcare.com>
2023-10-30 11:30:40 +0200

    feat(AOE-1401): added variants

--- packages/react/src/stories/Container/Container.stories.tsx
+++ packages/react/src/stories/Container/Container.stories.tsx
@@ -1,8 +1,18 @@
 import { Meta, Story } from '@storybook/react';
 import Container, { ContainerProps } from '../../components/Container';
+import TextInput from '../../components/TextInput';
 import { DESCRIPTION, DO, DONT, TITLE } from './ContainerInfo';
 import InfoTemplate from '../InfoTemplate/InfoTemplate';
 import Typography from '../../components/Typography';
+import * as StoryTitle from '../StoryTitle';
+import styled from 'styled-components';
+import { Theme } from '../../types';
+
+const StyledContainer = styled(Container)`
+  ${({ theme }: Theme) => `
+    background-color: ${theme.ColorNeutralWhite}
+  `}
+`;
 
 export default {
   title: 'React/Components/Container',
@@ -20,10 +30,29 @@ export const Overview = () => {
   );
 };
 
+export const Variants = () => {
+  return (
+    <Container direction="column" gap="SpacingBase32">
+      <StoryTitle.Overview>Container with margin</StoryTitle.Overview>
+      <StyledContainer m="SpacingBase12">Lorem ipsum</StyledContainer>
+
+      <StoryTitle.Overview>Container with padding</StoryTitle.Overview>
+      <StyledContainer p="SpacingBase12">Lorem ipsum</StyledContainer>
+
+      <StoryTitle.Overview>
+        Flexbox container with spacing and padding
+      </StoryTitle.Overview>
+      <StyledContainer direction="column" gap="SpacingBase12" p="SpacingBase12">
+        <TextInput label="Field A" />
+        <TextInput label="Field B" />
+        <TextInput label="Field C" />
+      </StyledContainer>
+    </Container>
+  );
+};
+
 const Template = ({ children, ...rest }: ContainerProps) => (
-  <div style={{ backgroundColor: 'white' }}>
-    <Container {...rest}>{children}</Container>
-  </div>
+  <StyledContainer {...rest}>{children}</StyledContainer>
 );
 
 export const Interactive: Story<ContainerProps> = Template.bind({});
