476f0d5cedf7cd5a48b9a17595ed449e50632cd6
aapostolou <alexandros.apostolou@newcrosshealthcare.com>
2023-11-03 17:00:00 +0200

    feat(OAE-1401): moved type to be a global one

--- packages/react/src/types/index.ts
+++ packages/react/src/types/index.ts
@@ -7,3 +7,20 @@ export type Theme = {
 };
 export type ExtendedTheme<T> = Theme & T;
 export type Mode = 'dark' | 'light';
+
+export type ThemeSpacing = keyof Pick<
+  ThemeDesignTokens,
+  | 'SpacingBase0'
+  | 'SpacingBase4'
+  | 'SpacingBase8'
+  | 'SpacingBase12'
+  | 'SpacingBase16'
+  | 'SpacingBase24'
+  | 'SpacingBase32'
+  | 'SpacingBase40'
+  | 'SpacingBase48'
+  | 'SpacingBase56'
+  | 'SpacingBase64'
+  | 'SpacingBase72'
+  | 'SpacingBase80'
+>;
