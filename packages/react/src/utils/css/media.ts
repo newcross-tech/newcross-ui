27942c420fd65eb5969b3b789936940b78ce590b
Vangelis-Vlassopoulos <Vangelis.vlassopoulos@newcrosshealthcare.com>
2024-10-15 13:24:06 +0300

    feat(ASK-0000): correct theme type in media util

--- packages/react/src/utils/css/media.ts
+++ packages/react/src/utils/css/media.ts
@@ -5,26 +5,22 @@ type Args = Parameters<typeof css>;
 
 export const media = {
   sm: (...args: Args) => css`
-    @media (max-width: ${({ theme }: { theme: Theme }) =>
-        theme.BreakpointsSm}px) {
+    @media (max-width: ${({ theme }: Theme) => theme.BreakpointsSm}px) {
       ${css(...args)};
     }
   `,
   md: (...args: Args) => css`
-    @media (max-width: ${({ theme }: { theme: Theme }) =>
-        theme.BreakpointsMd}px) {
+    @media (max-width: ${({ theme }: Theme) => theme.BreakpointsMd}px) {
       ${css(...args)};
     }
   `,
   lg: (...args: Args) => css`
-    @media (max-width: ${({ theme }: { theme: Theme }) =>
-        theme.BreakpointsLg}px) {
+    @media (max-width: ${({ theme }: Theme) => theme.BreakpointsLg}px) {
       ${css(...args)};
     }
   `,
   xl: (...args: Args) => css`
-    @media (max-width: ${({ theme }: { theme: Theme }) =>
-        theme.BreakpointsXl}px) {
+    @media (max-width: ${({ theme }: Theme) => theme.BreakpointsXl}px) {
       ${css(...args)};
     }
   `,
