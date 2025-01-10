e55315305cce6d5d8ba0a7708345959c40e57f53
Kevin Zino <126091262+KevinZIno@users.noreply.github.com>
2025-01-10 13:29:17 +0200

    chore(HFAUI-53): update checkbox component

--- packages/react/src/components/Checkbox/Checkbox.types.ts
+++ packages/react/src/components/Checkbox/Checkbox.types.ts
@@ -1,10 +1,46 @@
-import { CheckboxProps } from './Checkbox';
+import { ReactNode } from 'react';
+import { TestProp } from '../../types';
 
 export type CheckboxType = 'indeterminate';
 
-export type CheckboxPropsExtended = CheckboxProps & {
-  selected?: boolean;
-};
-
-export type hasErrorProps = Pick<CheckboxPropsExtended, 'type' | 'selected'>;
-export type isSelectedProps = Pick<CheckboxProps, 'type'>;
+/**
+ * @private The props after normalization
+ */
+export type CheckboxPropsStrict = {
+  /**
+   * Sets the type the checkbox is - check or indeterminate
+   */
+  type?: CheckboxType;
+  /**
+   * Label for the checkbox
+   */
+  label?: ReactNode;
+  /**
+   * Whether the press behavior is disabled.
+   */
+  disabled: boolean;
+  /**
+   * Shows different styles when error is true
+   */
+  hasError?: boolean;
+  /**
+   * Callback fired when the state is changed.
+   */
+  onChange: (
+    event: React.MouseEvent<HTMLDivElement> | React.KeyboardEvent<HTMLElement>
+  ) => void;
+  /**
+   * Callback fired when the state is changed.
+   */
+  onClick: (
+    event: React.MouseEvent<HTMLDivElement> | React.KeyboardEvent<HTMLElement>
+  ) => void;
+  /**
+   * Determines selected/checked state
+   */
+  checked: boolean;
+  /**
+   * Flag to enable/disable accessibility
+   */
+  allowTab: boolean;
+} & TestProp;
