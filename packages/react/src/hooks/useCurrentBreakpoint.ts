be5365e55663b949a491b0a9999360ee7fb2c049
Peter Popov <petar.popov@newcrosshealthcare.com>
2025-01-16 16:44:49 +0200

    feat: implement useCurrentBreakpoint

---
+++ packages/react/src/hooks/useCurrentBreakpoint.ts
@@ -0,0 +1,28 @@
+import useTheme from './useTheme';
+import { useWindowSize } from './useWindowSize';
+import { useMemo } from 'react';
+import { Breakpoint, getBreakpointDesignToken } from '../utils/css/breakpoint';
+
+export function useCurrentBreakpoint(): Breakpoint | undefined {
+  const theme = useTheme();
+  const { width: windowWidth } = useWindowSize();
+
+  return useMemo(() => {
+    if (windowWidth === undefined) {
+      return undefined;
+    }
+
+    const breakpointNameToValue = (breakpoint: Breakpoint) => ({
+      name: breakpoint,
+      value: theme[getBreakpointDesignToken(breakpoint)],
+    });
+    const breakpoints = Object.values(Breakpoint)
+      .map(breakpointNameToValue)
+      .sort((a, b) => a.value - b.value);
+    const lastBreakpoint = breakpoints.pop()!;
+    return (
+      breakpoints.find((breakpoint) => windowWidth <= breakpoint.value)?.name ??
+      lastBreakpoint.name
+    );
+  }, [windowWidth]);
+}
--- packages/react/src/types/utility-types.ts
+++ packages/react/src/types/utility-types.ts
@@ -2,3 +2,9 @@ export type RequiredProps<T, K extends keyof T> = Omit<T, K> &
   Required<Pick<T, K>>;
 export type OptionalProps<T, K extends keyof T> = Omit<T, K> &
   Partial<Pick<T, K>>;
+
+/**
+ * Helper for writing `ValueFrom<typeof SomeConstant>` instead of `typeof SomeConstant[keyof typeof SomeConstant]`.
+ * Also makes using anonymous types easier -> Instead of `SomeOtherType<{ a: number }>[keyof SomeOtherType<{ a: number }>]` you can write `ValueFrom<SomeOtherType<{ a: number }>>`.
+ */
+export type ValueFrom<T> = T[keyof T];
