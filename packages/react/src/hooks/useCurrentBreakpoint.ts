c4e4246ddb27b95b1397289142f5989a085da35c
Peter Popov <petar.popov@newcrosshealthcare.com>
2025-01-16 18:35:31 +0200

    fix: replace non-null assertion with guard

--- packages/react/src/hooks/useCurrentBreakpoint.ts
+++ packages/react/src/hooks/useCurrentBreakpoint.ts
@@ -16,10 +16,16 @@ export function useCurrentBreakpoint(): Breakpoint | undefined {
       name: breakpoint,
       value: theme[getBreakpointDesignToken(breakpoint)],
     });
+
     const breakpoints = Object.values(Breakpoint)
       .map(breakpointNameToValue)
       .sort((a, b) => a.value - b.value);
-    const lastBreakpoint = breakpoints.pop()!;
+
+    const lastBreakpoint = breakpoints.pop();
+    if (!lastBreakpoint) {
+      throw new Error('There should be at least one breakpoint.');
+    }
+
     return (
       breakpoints.find((breakpoint) => windowWidth <= breakpoint.value)?.name ??
       lastBreakpoint.name
