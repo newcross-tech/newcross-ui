d3bdf0058357f79d9d49aa5ae9a3fe7a8f95aef9
Vangelis-Vlassopoulos <Vangelis.vlassopoulos@newcrosshealthcare.com>
2025-01-24 12:59:54 +0200

    chore(HFAUI-138): rework button and add hover styles

--- packages/react/src/components/Button/Button.style.ts
+++ packages/react/src/components/Button/Button.style.ts
@@ -1,74 +1,102 @@
-import styled, { css, FlattenSimpleInterpolation } from 'styled-components';
+import styled from 'styled-components';
 import { ThemeDesignTokens } from '../../theme/ThemeProvider';
-import { ExtendedTheme } from '../../types';
 import { getIconSize, getTabbedStateStyles } from '../../utils/css';
-import { ButtonProps, TypographyValues } from './Button';
-import { ButtonScheme, ButtonSizes, ButtonVariant } from './Button.types';
+import { TypographyValues } from './Button';
+import { ButtonPropsStrict, ButtonScheme, ButtonVariant } from './Button.types';
 import Container from '../Container';
 
-const getBorderStyle = (theme: ThemeDesignTokens, color: string) => css`
-  border: ${theme.BorderBaseWidthSm} solid ${color};
-`;
-
-const variantStyles = (
-  theme: ThemeDesignTokens,
-  scheme: ButtonScheme
-): Record<ButtonVariant, FlattenSimpleInterpolation> => ({
-  primary: css`
-    color: ${theme.ElementsTextActionPrimaryDark};
-    background-color: ${theme.ElementsSurfaceActionDefault};
-    ${getBorderStyle(theme, theme.ElementsBorderActionDefault)};
-  `,
-  secondary: css`
-    color: ${scheme === 'light' ? theme.ElementsTextActionPrimaryDark : theme.ElementsTextActionSecondaryLight};
-    background-color: transparent;
-    ${getBorderStyle(theme, theme.ElementsBorderActionDefault)};
-  `,
-  danger: css`
-    color: ${theme.ElementsTextActionDanger};
-    background-color: ${theme.ElementsSurfaceActionDanger};
-    ${getBorderStyle(theme, theme.ElementsSurfaceActionDanger)};
-  `,
+const getBorderStyle = (theme: ThemeDesignTokens, color: string) => ({
+  border: `${theme.BorderBaseWidthSm} solid ${color}`,
 });
 
-const disabledStyles = (theme: ThemeDesignTokens, variant: ButtonVariant): FlattenSimpleInterpolation => css`
-  cursor: default;
-  color: ${theme.ElementsTextDisabled};
-  background-color: ${variant === 'secondary' ? 'transparent' : theme.ElementsSurfaceDisabled};
-  ${getBorderStyle(theme, theme.ElementsSurfaceDisabled)};
-`;
-
-const activeStyles = (theme: ThemeDesignTokens): Record<ButtonVariant, FlattenSimpleInterpolation> => ({
-  primary: css`
-    background-color: ${theme.ElementsSurfaceActionHover};
-    ${getBorderStyle(theme, theme.ElementsSurfaceActionHover)};
-  `,
-  secondary: css`
-    color: ${theme.ElementsTextActionPrimaryDark};
-    background-color: ${theme.ElementsSurfaceActionHoverSecondary};
-    ${getBorderStyle(theme, theme.ElementsBorderActionDefault)};
-  `,
-  danger: css`
-    background-color: ${theme.ElementsSurfaceActionDangerHover};
-    ${getBorderStyle(theme, theme.ElementsSurfaceActionDangerHover)};
-  `,
+const getHoverStyles = (theme: ThemeDesignTokens) => ({
+  primary: {
+    ':hover': { backgroundColor: theme.ElementsSurfaceActionHover, borderColor: theme.ElementsSurfaceActionHover },
+  },
+  secondary: {
+    ':hover': {
+      backgroundColor: theme.ElementsSurfaceActionHoverSecondary,
+      borderColor: theme.ElementsBorderActionDefault,
+      color: theme.ElementsTextActionPrimaryDark,
+    },
+  },
+  danger: {
+    ':hover': {
+      backgroundColor: theme.ElementsSurfaceActionDangerHover,
+      borderColor: theme.ElementsSurfaceActionDangerHover,
+    },
+  },
 });
 
-export const IconWrapper = styled(Container)<ExtendedTheme<{ size?: ButtonSizes }>>`
-  ${({ theme, size = 'large' }) => getIconSize(theme, TypographyValues[size], 'fontSize')};
-`;
+const variantStyles = (theme: ThemeDesignTokens, scheme: ButtonScheme) => ({
+  primary: {
+    color: theme.ElementsTextActionPrimaryDark,
+    backgroundColor: theme.ElementsSurfaceActionDefault,
+    ...getBorderStyle(theme, theme.ElementsBorderActionDefault),
+  },
+  secondary: {
+    color: scheme === 'light' ? theme.ElementsTextActionPrimaryDark : theme.ElementsTextActionSecondaryLight,
+    backgroundColor: 'transparent',
+    ...getBorderStyle(theme, theme.ElementsBorderActionDefault),
+  },
+  danger: {
+    color: theme.ElementsTextActionDanger,
+    backgroundColor: theme.ElementsSurfaceActionDanger,
+    ...getBorderStyle(theme, theme.ElementsSurfaceActionDanger),
+  },
+});
 
-export const Button = styled(Container)<ExtendedTheme<Pick<ButtonProps, 'variant' | 'scheme' | 'disabled'>>>`
-  ${({ theme, variant = 'primary', scheme = 'light', disabled }) => css`
-    cursor: ${disabled ? 'default' : 'pointer'};
-    border-radius: ${theme.BorderBaseRadiusRounded};
+const disabledStyles = (theme: ThemeDesignTokens, variant: ButtonVariant) => ({
+  cursor: 'default',
+  color: theme.ElementsTextDisabled,
+  backgroundColor: variant === 'secondary' ? 'transparent' : theme.ElementsSurfaceDisabled,
+  ...getBorderStyle(theme, theme.ElementsSurfaceDisabled),
+});
 
-    ${getTabbedStateStyles()};
+const activeStyles = (theme: ThemeDesignTokens) => ({
+  primary: {
+    backgroundColor: theme.ElementsSurfaceActionHover,
+    ...getBorderStyle(theme, theme.ElementsSurfaceActionHover),
+  },
+  secondary: {
+    color: theme.ElementsTextActionPrimaryDark,
+    backgroundColor: theme.ElementsSurfaceActionHoverSecondary,
+    ...getBorderStyle(theme, theme.ElementsBorderActionDefault),
+  },
+  danger: {
+    backgroundColor: theme.ElementsSurfaceActionDangerHover,
+    ...getBorderStyle(theme, theme.ElementsSurfaceActionDangerHover),
+  },
+});
 
-    ${disabled ? disabledStyles(theme, variant) : variantStyles(theme, scheme)[variant]}
+export const IconWrapper = styled(Container)<Pick<ButtonPropsStrict, 'size'>>(({ theme, size }) =>
+  getIconSize(theme, TypographyValues[size], 'fontSize')
+);
 
-    &:active {
-      ${!disabled && activeStyles(theme)[variant]};
-    }
-  `}
-`;
+export const Button = styled(Container)<Pick<ButtonPropsStrict, 'variant' | 'scheme' | 'disabled'>>(
+  ({ theme, variant, scheme, disabled }) => {
+    const baseStyles = {
+      cursor: disabled ? 'default' : 'pointer',
+      borderRadius: theme.BorderBaseRadiusRounded,
+      ...getTabbedStateStyles(),
+    };
+
+    const stateStyles = disabled
+      ? disabledStyles(theme, variant)
+      : { ...variantStyles(theme, scheme)[variant], ...getHoverStyles(theme)[variant] };
+
+    const activeStateStyles = !disabled
+      ? {
+          '&:active': {
+            ...activeStyles(theme)[variant],
+          },
+        }
+      : {};
+
+    return {
+      ...baseStyles,
+      ...stateStyles,
+      ...activeStateStyles,
+    };
+  }
+);
