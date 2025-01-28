81c999f129b921f6748985bdb53323ac164b4040
Vangelis-Vlassopoulos <Vangelis.vlassopoulos@newcrosshealthcare.com>
2025-01-28 15:38:06 +0200

    chore(HFAUI-138): add transition on hover

--- packages/react/src/components/Button/Button.style.ts
+++ packages/react/src/components/Button/Button.style.ts
@@ -1,102 +1,126 @@
 import styled from 'styled-components';
-import { ThemeDesignTokens } from '../../theme/ThemeProvider';
 import { getIconSize, getTabbedStateStyles } from '../../utils/css';
 import { TypographyValues } from './Button';
-import { ButtonPropsStrict, ButtonScheme, ButtonVariant } from './Button.types';
+import { ButtonPropsStrict } from './Button.types';
 import Container from '../Container';
+import { Theme } from '../../types';
 
-const getBorderStyle = (theme: ThemeDesignTokens, color: string) => ({
+const getBorderStyle = ({ theme, color }: Theme & { color: string }) => ({
   border: `${theme.BorderBaseWidthSm} solid ${color}`,
 });
 
-const getHoverStyles = (theme: ThemeDesignTokens) => ({
-  primary: {
-    ':hover': { backgroundColor: theme.ElementsSurfaceActionHover, borderColor: theme.ElementsSurfaceActionHover },
-  },
-  secondary: {
-    ':hover': {
-      backgroundColor: theme.ElementsSurfaceActionHoverSecondary,
-      borderColor: theme.ElementsBorderActionDefault,
+const getHoverStyles = ({ theme, variant }: Theme & Pick<ButtonPropsStrict, 'variant'>) => {
+  return {
+    primary: {
+      ':hover': {
+        backgroundColor: theme.ElementsSurfaceActionHover,
+        borderColor: theme.ElementsSurfaceActionHover,
+      },
+    },
+    secondary: {
+      ':hover': {
+        backgroundColor: theme.ElementsSurfaceActionHoverSecondary,
+        borderColor: theme.ElementsBorderActionDefault,
+        color: theme.ElementsTextActionPrimaryDark,
+      },
+    },
+    danger: {
+      ':hover': {
+        backgroundColor: theme.ElementsSurfaceActionDangerHover,
+        borderColor: theme.ElementsSurfaceActionDangerHover,
+      },
+    },
+  }[variant];
+};
+
+const getVariantStyles = ({ theme, scheme, variant }: Theme & Pick<ButtonPropsStrict, 'variant' | 'scheme'>) => {
+  return {
+    primary: {
       color: theme.ElementsTextActionPrimaryDark,
+      backgroundColor: theme.ElementsSurfaceActionDefault,
+      ...getBorderStyle({ theme, color: theme.ElementsBorderActionDefault }),
     },
-  },
-  danger: {
-    ':hover': {
+    secondary: {
+      color: scheme === 'light' ? theme.ElementsTextActionPrimaryDark : theme.ElementsTextActionSecondaryLight,
+      backgroundColor: 'transparent',
+      ...getBorderStyle({ theme, color: theme.ElementsBorderActionDefault }),
+    },
+    danger: {
+      color: theme.ElementsTextActionDanger,
+      backgroundColor: theme.ElementsSurfaceActionDanger,
+      ...getBorderStyle({ theme, color: theme.ElementsSurfaceActionDanger }),
+    },
+  }[variant];
+};
+
+const getDisabledStyles = ({ theme, variant }: Theme & Pick<ButtonPropsStrict, 'variant'>) => {
+  return {
+    primary: {
+      cursor: 'default',
+      color: theme.ElementsTextDisabled,
+      backgroundColor: theme.ElementsSurfaceDisabled,
+      ...getBorderStyle({ theme, color: theme.ElementsSurfaceDisabled }),
+    },
+    secondary: {
+      cursor: 'default',
+      color: theme.ElementsTextDisabled,
+      backgroundColor: 'transparent',
+      ...getBorderStyle({ theme, color: theme.ElementsSurfaceDisabled }),
+    },
+    danger: {
+      cursor: 'default',
+      color: theme.ElementsTextDisabled,
+      backgroundColor: theme.ElementsSurfaceDisabled,
+      ...getBorderStyle({ theme, color: theme.ElementsSurfaceDisabled }),
+    },
+  }[variant];
+};
+
+const getActiveStyles = ({ theme, variant }: Theme & Pick<ButtonPropsStrict, 'variant'>) => {
+  return {
+    primary: {
+      backgroundColor: theme.ElementsSurfaceActionHover,
+      ...getBorderStyle({ theme, color: theme.ElementsSurfaceActionHover }),
+    },
+    secondary: {
+      color: theme.ElementsTextActionPrimaryDark,
+      backgroundColor: theme.ElementsSurfaceActionHoverSecondary,
+      ...getBorderStyle({ theme, color: theme.ElementsBorderActionDefault }),
+    },
+    danger: {
       backgroundColor: theme.ElementsSurfaceActionDangerHover,
-      borderColor: theme.ElementsSurfaceActionDangerHover,
+      ...getBorderStyle({ theme, color: theme.ElementsSurfaceActionDangerHover }),
     },
-  },
-});
-
-const variantStyles = (theme: ThemeDesignTokens, scheme: ButtonScheme) => ({
-  primary: {
-    color: theme.ElementsTextActionPrimaryDark,
-    backgroundColor: theme.ElementsSurfaceActionDefault,
-    ...getBorderStyle(theme, theme.ElementsBorderActionDefault),
-  },
-  secondary: {
-    color: scheme === 'light' ? theme.ElementsTextActionPrimaryDark : theme.ElementsTextActionSecondaryLight,
-    backgroundColor: 'transparent',
-    ...getBorderStyle(theme, theme.ElementsBorderActionDefault),
-  },
-  danger: {
-    color: theme.ElementsTextActionDanger,
-    backgroundColor: theme.ElementsSurfaceActionDanger,
-    ...getBorderStyle(theme, theme.ElementsSurfaceActionDanger),
-  },
-});
-
-const disabledStyles = (theme: ThemeDesignTokens, variant: ButtonVariant) => ({
-  cursor: 'default',
-  color: theme.ElementsTextDisabled,
-  backgroundColor: variant === 'secondary' ? 'transparent' : theme.ElementsSurfaceDisabled,
-  ...getBorderStyle(theme, theme.ElementsSurfaceDisabled),
-});
-
-const activeStyles = (theme: ThemeDesignTokens) => ({
-  primary: {
-    backgroundColor: theme.ElementsSurfaceActionHover,
-    ...getBorderStyle(theme, theme.ElementsSurfaceActionHover),
-  },
-  secondary: {
-    color: theme.ElementsTextActionPrimaryDark,
-    backgroundColor: theme.ElementsSurfaceActionHoverSecondary,
-    ...getBorderStyle(theme, theme.ElementsBorderActionDefault),
-  },
-  danger: {
-    backgroundColor: theme.ElementsSurfaceActionDangerHover,
-    ...getBorderStyle(theme, theme.ElementsSurfaceActionDangerHover),
-  },
-});
+  }[variant];
+};
 
 export const IconWrapper = styled(Container)<Pick<ButtonPropsStrict, 'size'>>(({ theme, size }) =>
   getIconSize(theme, TypographyValues[size], 'fontSize')
 );
 
-export const Button = styled(Container)<Pick<ButtonPropsStrict, 'variant' | 'scheme' | 'disabled'>>(
-  ({ theme, variant, scheme, disabled }) => {
-    const baseStyles = {
-      cursor: disabled ? 'default' : 'pointer',
-      borderRadius: theme.BorderBaseRadiusRounded,
-      ...getTabbedStateStyles(),
-    };
+export const Button = styled(Container)<Pick<ButtonPropsStrict, 'variant' | 'scheme' | 'disabled'>>((props) => {
+  const baseStyles = {
+    cursor: props.disabled ? 'default' : 'pointer',
+    borderRadius: props.theme.BorderBaseRadiusRounded,
+    ...getTabbedStateStyles(),
+  };
 
-    const stateStyles = disabled
-      ? disabledStyles(theme, variant)
-      : { ...variantStyles(theme, scheme)[variant], ...getHoverStyles(theme)[variant] };
+  const stateStyles = props.disabled
+    ? getDisabledStyles(props)
+    : { ...getVariantStyles(props), ...getHoverStyles(props) };
 
-    const activeStateStyles = !disabled
-      ? {
-          '&:active': {
-            ...activeStyles(theme)[variant],
-          },
-        }
-      : {};
+  const activeStateStyles = !props.disabled
+    ? {
+        '&:active': {
+          ...getActiveStyles(props),
+        },
+      }
+    : {};
 
-    return {
-      ...baseStyles,
-      ...stateStyles,
-      ...activeStateStyles,
-    };
-  }
-);
+  return {
+    ...baseStyles,
+    ...stateStyles,
+    ...activeStateStyles,
+    ...{ transition: 'background-color 0.2s ease-in-out', tramsition: 'color 0.2s ease-in-out' },
+  };
+});
