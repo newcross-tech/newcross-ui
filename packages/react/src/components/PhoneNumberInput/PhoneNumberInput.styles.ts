081424634e28af3bbe124953259f77b1fd88c136
Vangelis-Vlassopoulos <Vangelis.vlassopoulos@newcrosshealthcare.com>
2025-01-29 16:09:06 +0200

    feat(HFAUI-72): update phone number input

--- packages/react/src/components/PhoneNumberInput/PhoneNumberInput.styles.ts
+++ packages/react/src/components/PhoneNumberInput/PhoneNumberInput.styles.ts
@@ -1,82 +1,127 @@
-import { ExtendedTheme } from '../../types';
-import styled, { css } from 'styled-components';
-import Label from '../Label';
-import Typography from '../Typography';
+import styled from 'styled-components';
+import Container from '../Container';
+import { PhoneNumberInputPropsStrict } from './PhoneNumberInput.types';
+import {
+  getCommonStateStyles,
+  getFocusedStyles,
+} from '../TextInput/TextInput.style';
+import { getScrollbarStyles } from '../../utils/css/getScrollbarStyles';
 
-export const Content = styled.div`
-  ${({ $isError, theme }: ExtendedTheme<{ $isError: boolean }>) => css`
-    min-height: ${theme.SpacingBase48};
+export const Content = styled(Container)<
+  Pick<PhoneNumberInputPropsStrict, 'isError' | 'disabled' | 'isValid'> & {
+    isFocused: boolean;
+  }
+>(({ theme, isError, isFocused, disabled, isValid }) => ({
+  minHeight: theme.BaselineSpacesSpace48,
 
-    .react-international-phone-input-container {
-      border: ${theme.SizeBaseBorderWidthSm} solid
-        ${$isError ? theme.ColorBaseRed100 : theme.ColorBaseGrey200};
-      border-radius: ${theme.CardBorderRadius};
-    }
+  '.react-international-phone-input-container': {
+    ...getCommonStateStyles({
+      theme,
+      hasError: isError,
+      disabled,
+      isValid,
+    }),
+    ...(isFocused && getFocusedStyles({ theme })),
+    borderRadius: theme.BorderBaseRadiusMd,
+  },
 
-    .react-international-phone-country-selector-button__button-content {
-      padding: ${theme.SpacingBase0} ${theme.SpacingBase8};
-    }
+  '.react-international-phone-country-selector-button__button-content': {
+    padding: `${theme.BaselineSpacesSpace0} ${theme.BaselineSpacesSpace8} ${theme.BaselineSpacesSpace0} ${theme.BaselineSpacesSpace16}`,
+    '> img': {
+      marginRight: theme.BaselineSpacesSpace8,
+    },
+  },
 
-    .react-international-phone-country-selector-button {
-      border: ${theme.SpacingBase0};
-      position: relative;
-      border-radius: ${theme.CardBorderRadius} ${theme.SpacingBase0}
-        ${theme.SpacingBase0} ${theme.CardBorderRadius}!important;
-      height: ${theme.SpacingBase48};
-    }
+  '.react-international-phone-country-selector-button': {
+    border: theme.BaselineSpacesSpace0,
+    position: 'relative',
+    borderRadius: `${theme.BorderBaseRadiusMd} ${theme.BaselineSpacesSpace0} ${theme.BaselineSpacesSpace0} ${theme.BorderBaseRadiusMd}!important`,
+    height: theme.BaselineSpacesSpace48,
+  },
 
-    .react-international-phone-country-selector-button::after {
-      content: '';
-      position: absolute;
-      top: 25%;
-      right: ${theme.SpacingBase0};
-      height: 50%;
-      width: ${theme.SizeBaseBorderWidthSm};
-      background-color: ${theme.ColorNeutralGrey200};
-    }
+  '.react-international-phone-country-selector-button::after': {
+    content: "''",
+    position: 'absolute',
+    top: '25%',
+    right: theme.BaselineSpacesSpace0,
+    height: '50%',
+    width: theme.BorderBaseWidthSm,
+    backgroundColor: theme.ElementsBorderDefault,
+  },
 
-    .react-international-phone-input {
-      color: ${theme.TypographyColorSecondary}!important;
-      width: 100%;
-      border: ${theme.SpacingBase0} !important;
-      border-radius: ${theme.SpacingBase0} ${theme.CardBorderRadius}
-        ${theme.CardBorderRadius} ${theme.SpacingBase0}!important;
-      height: ${theme.SpacingBase48};
-    }
+  '.react-international-phone-input': {
+    color: theme.ElementsTextDefaultDark,
+    width: '100%',
+    border: `${theme.BaselineSpacesSpace0} !important`,
+    borderRadius: `${theme.BaselineSpacesSpace0} ${theme.BorderBaseRadiusMd} ${theme.BorderBaseRadiusMd} ${theme.BaselineSpacesSpace0}!important`,
+    height: theme.BaselineSpacesSpace48,
+  },
 
-    .react-international-phone-country-selector-button__dropdown-arrow {
-      border-bottom: 2px solid ${theme.ColorNeutralGrey100};
-      border-right: 2px solid ${theme.ColorNeutralGrey100};
-      width: ${theme.SpacingBase8};
-      height: ${theme.SpacingBase8};
-      transform: rotate(45deg);
-      margin-left: ${theme.SpacingBase4};
-      margin-bottom: ${theme.SpacingBase4};
-      transition: all 0.1s ease-out;
-      border-left: ${theme.SpacingBase0};
-      border-top: ${theme.SpacingBase0};
-    }
+  '.react-international-phone-input--disabled': {
+    color: theme.ElementsTextDisabled,
+  },
 
-    .react-international-phone-country-selector-button__dropdown-arrow--active {
-      transform: rotate(-135deg);
-      margin-top: ${theme.SpacingBase8};
-    }
+  '.react-international-phone-country-selector-button__dropdown-arrow': {
+    borderBottom: `${theme.BorderBaseWidthSm} solid ${theme.ElementsTextDefaultDark}`,
+    borderRight: `${theme.BorderBaseWidthSm} solid ${theme.ElementsTextDefaultDark}`,
+    width: theme.BaselineSpacesSpace8,
+    height: theme.BaselineSpacesSpace8,
+    transform: 'rotate(45deg)',
+    marginLeft: theme.BaselineSpacesSpace4,
+    marginBottom: theme.BaselineSpacesSpace4,
+    transition: 'all 0.1s ease-out',
+    borderLeft: theme.BaselineSpacesSpace0,
+    borderTop: theme.BaselineSpacesSpace0,
+  },
+  '.react-international-phone-country-selector-button__dropdown-arrow--disabled':
+    {
+      borderBottom: `${theme.BorderBaseWidthSm} solid ${theme.ElementsTextDisabled}`,
+      borderRight: `${theme.BorderBaseWidthSm} solid ${theme.ElementsTextDisabled}`,
+    },
 
-    .react-international-phone-input,
-    .react-international-phone-country-selector-dropdown__list-item-country-name,
-    .react-international-phone-country-selector-dropdown__list-item-dial-code {
-      font-family: ${theme.TextInputFontFamily};
-      font-size: ${theme.TextInputFontSize};
-      line-height: ${theme.TextInputLineHeight};
-    }
-  `};
-`;
+  '.react-international-phone-country-selector-button__dropdown-arrow--active':
+    {
+      transform: 'rotate(-135deg)',
+      marginTop: theme.BaselineSpacesSpace8,
+    },
 
-export const LabelWithMargin = styled(Label)`
-  margin-bottom: ${({ theme }) => theme.SpacingBase4};
-`;
+  '.react-international-phone-input, .react-international-phone-country-selector-dropdown__list-item-country-name, .react-international-phone-country-selector-dropdown__list-item-dial-code':
+    {
+      fontFamily: theme.BaselineFontFontFamilyPoppinsRegular,
+      fontSize: theme.TypographyFontSize14,
+      lineHeight: theme.TypographyLineHeight20,
+    },
+  '.react-international-phone-country-selector-dropdown__list-item': {
+    height: theme.BaselineSpacesSpace48,
+    ':hover': {
+      backgroundColor: theme.ElementsSurfaceActionHover,
+      transition: 'background-color 0.2s ease-in-out',
+    },
+  },
+  '.react-international-phone-country-selector-dropdown__list-item--selected': {
+    backgroundColor: theme.ElementsSurfaceActionDefault,
+  },
+  '.react-international-phone-country-selector-dropdown': {
+    ...getScrollbarStyles({ theme }),
+  },
+  '.react-international-phone-input--disabled, .react-international-phone-country-selector-button--disabled':
+    {
+      backgroundColor: theme.ElementsSurfaceDisabled,
+    },
+}));
 
-export const ErrorMessage = styled(Typography)`
-  padding-left: ${({ theme }) => theme.SpacingBase16};
-  padding-top: ${({ theme }) => theme.SpacingBase4};
-`;
+export const PhoneInputWrapper = styled(Container)({
+  position: 'relative',
+  width: '100%',
+});
+
+export const IconWrapper = styled(Container)({
+  position: 'absolute',
+  right: '16px',
+  top: '50%',
+  transform: 'translateY(-50%)',
+});
+
+export const ClearIconWrapper = styled(IconWrapper)({
+  cursor: 'pointer',
+});
--- packages/react/src/components/PhoneNumberInput/PhoneNumberInput.tsx
+++ packages/react/src/components/PhoneNumberInput/PhoneNumberInput.tsx
@@ -1,44 +1,157 @@
-import { PhoneInput, PhoneInputProps } from 'react-international-phone';
+import { PhoneInput } from 'react-international-phone';
+import {
+  faCircleXmark,
+  faCircleExclamation,
+  faCircleCheck,
+} from '@fortawesome/pro-light-svg-icons';
 import * as Styled from './PhoneNumberInput.styles';
 import 'react-international-phone/style.css';
+import HelperText from '../TextInput/HelperText';
+import Container from '../Container';
+import Label from '../Label';
+import Typography from '../Typography';
+import { OptionalProps } from '../../types';
+import Icon from '../Icon';
+import { useState } from 'react';
+import { PhoneNumberInputPropsStrict } from './PhoneNumberInput.types';
 
-export type PhoneNumberInputProps = {
-  /**
-   * Gives phone number input a label
-   */
-  label?: string;
-  /**
-   * Gives phone number input an error state
-   */
-  isError?: boolean;
-  /**
-   * Gives phone number input an helper text
-   */
-  helperText?: string;
-} & PhoneInputProps;
+export type PhoneNumberInputProps = OptionalProps<
+  PhoneNumberInputPropsStrict,
+  'disabled' | 'isError' | 'isValid' | 'required'
+>;
 
-const PhoneNumberInput = ({
-  label,
-  isError,
-  helperText,
-  ...phoneInputProps
-}: PhoneNumberInputProps) => (
-  <Styled.Content $isError={!!isError}>
-    {label && (
-      <Styled.LabelWithMargin variant="subtitle1">
-        {label}
-      </Styled.LabelWithMargin>
-    )}
-    <PhoneInput {...phoneInputProps} />
-    {helperText && (
-      <Styled.ErrorMessage
-        variant="paragraph3"
-        color={isError ? 'error' : 'secondary'}
-      >
-        {helperText}
-      </Styled.ErrorMessage>
-    )}
-  </Styled.Content>
-);
+const normalizePhoneNumberInputProps = (
+  props: PhoneNumberInputProps,
+  setIsFocused: (value: boolean) => void
+): PhoneNumberInputPropsStrict => ({
+  ...props,
+  disabled: props.disabled ?? false,
+  isError: props.isError ?? false,
+  isValid: props.isValid ?? false,
+  required: props.required ?? false,
+  onClick: (event: React.MouseEvent<HTMLInputElement>) => {
+    props.onClick?.(event);
+    setIsFocused(true);
+  },
+  onBlur: (event: React.FocusEvent<HTMLInputElement>) => {
+    props.onBlur?.(event);
+    setIsFocused(false);
+  },
+  onFocus: (event: React.FocusEvent<HTMLInputElement>) => {
+    props.onFocus?.(event);
+    setIsFocused(true);
+  },
+});
+
+const PhoneNumberInput = (_props: PhoneNumberInputProps) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const {
+    label,
+    isError,
+    isValid,
+    helperText,
+    disabled,
+    required,
+    testID,
+    ...phoneInputProps
+  } = normalizePhoneNumberInputProps(_props, setIsFocused);
+
+  const getTextColor = (disabled: boolean, hasError: boolean) => {
+    if (disabled) return 'disabled';
+    if (hasError) return 'dangerError';
+
+    return 'defaultDark';
+  };
+
+  const phoneInputId = `phone-input-component-${testID}`;
+  const preventEventPropagation = (event: React.MouseEvent) => {
+    event.preventDefault();
+    event.stopPropagation();
+  };
+  return (
+    <Styled.Content
+      isError={isError}
+      isFocused={isFocused}
+      isValid={isValid}
+      disabled={disabled}
+      flexDirection="column"
+      gap="xs"
+    >
+      {label && (
+        <Container gap="xs">
+          <Label
+            htmlFor={phoneInputId}
+            variant="h3"
+            color={getTextColor(disabled, isError)}
+            testID={`${phoneInputId}-label`}
+          >
+            {label}
+          </Label>
+          {required && !disabled && (
+            <Typography
+              testID={`${phoneInputId}-required-indicator`}
+              variant="h3"
+              color="dangerError"
+            >
+              *
+            </Typography>
+          )}
+        </Container>
+      )}
+      <Styled.PhoneInputWrapper display="block">
+        <PhoneInput disabled={disabled} {...phoneInputProps} />
+        {isValid && !disabled && !isFocused && !isError && (
+          <Styled.IconWrapper
+            testID={`${phoneInputId}-validation-check`}
+            justifyContent="center"
+          >
+            <Icon icon={faCircleCheck} variant="p1" color="successStandalone" />
+          </Styled.IconWrapper>
+        )}
+        {isError && !disabled && !isValid && !isFocused && (
+          <Styled.IconWrapper
+            testID={`${phoneInputId}-error-check`}
+            justifyContent="center"
+          >
+            <Icon icon={faCircleExclamation} variant="p1" color="dangerError" />
+          </Styled.IconWrapper>
+        )}
+        {isFocused && (
+          <Styled.IconWrapper
+            testID={`${phoneInputId}-clear-icon`}
+            justifyContent="center"
+            /**
+             * Prevents the input from losing focus when clicking on the clear icon
+             * Preventing on the onClick does not work as expected, hence using onMouseDown
+             */
+            onMouseDown={(event) => preventEventPropagation(event)}
+            onClick={() =>
+              phoneInputProps.onChange?.('', {
+                country: {
+                  name: '',
+                  iso2: '',
+                  dialCode: '',
+                  format: '',
+                  priority: 0,
+                  areaCodes: [],
+                },
+                inputValue: '',
+              })
+            }
+          >
+            <Icon icon={faCircleXmark} variant="p1" color="actionPrimaryDark" />
+          </Styled.IconWrapper>
+        )}
+      </Styled.PhoneInputWrapper>
+      {helperText && (
+        <HelperText
+          helperText={helperText}
+          errorText={isError ? helperText : ''}
+          testID={phoneInputId}
+        />
+      )}
+    </Styled.Content>
+  );
+};
 
 export default PhoneNumberInput;
